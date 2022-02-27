79f0c57e96ac5792d2bf6fd95d0599562c5896cd
zoocasso <zoocasso1590@gmail.com>
2022-02-27 23:45:18 +0900

    created recursion

---
+++ recursion/10870/10870.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('./input.txt').toString();
+
+function fibonacci(n) {
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else return fibonacci(n-1) + fibonacci(n-2);
+}
+
+console.log(fibonacci(input));
